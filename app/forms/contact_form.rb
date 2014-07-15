d991d7fca0378a20aafbeb94dabfffddcd85fe24
Mike Rogers <mike@kyanmedia.com>
2014-07-15 13:38:01 +0100

    removing base object

--- app/forms/contact_form.rb
+++ app/forms/contact_form.rb
@@ -1,4 +1,4 @@
-class ContactForm < FormObjectBase
+class ContactForm 
   include ActiveModel::Model
 
   attr_accessor :name, :email, :message
@@ -19,4 +19,17 @@ class ContactForm < FormObjectBase
     Column.new(attribute, column_type, limit)
   end
 
+  def self.new_with_defaults attributes={}
+    default_attributes = {}
+    new default_attributes.merge(attributes)
+  end
+
+  # From https://github.com/plataformatec/simple_form/blob/master/test/support/models.rb#L3-L8
+  # Allows SimpleForm to pick suitable input types.
+  Column = Struct.new(:name, :type, :limit) do
+    # Returns +true+ if the column is either of type integer, float or decimal.
+    def number?
+      type == :integer || type == :float || type == :decimal
+    end
+  end
 end
